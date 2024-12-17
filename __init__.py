33b2225052f50fbb325981eab6830aa9a0fec91e
Rodolfo Alberto Reyes Corona <rreyesc1500@alumno.ipn.mx>
2024-12-17 16:52:35 -0600

    Ant Colony Optimization Algorithm

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 11:25:12 2024
+
+@author: Rodolfo Alberto Reyes Corona
+"""
+
+from App_ACO import App_ACO
+
+if __name__ == "__main__":
+    aco = App_ACO()
+    aco.run()
